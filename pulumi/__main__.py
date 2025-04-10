b85b52807e5dfaf8a08c83cc6968f887a0e4f399
Marius Guščius <marius.guscius@mailbox.org>
2025-04-10 12:07:10 +0300

    explicit provider

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,18 +1,22 @@
 from os import getenv
 
-from pulumi import RunError
+from pulumi import RunError, ResourceOptions
+from pulumiverse_zitadel import Provider, MachineUser
 
 if not (ZITADEL_DOMAIN := getenv("ZITADEL_DOMAIN")):
     raise RunError(f"Environment variable 'ZITADEL_DOMAIN' is not set.")
 if not (ZITADEL_JWT_PROFILE_FILE := getenv("ZITADEL_JWT_PROFILE_FILE")):
     raise RunError(f"Environment variable 'ZITADEL_JWT_PROFILE_FILE' is not set.")
 
-# print(ZITADEL_DOMAIN, ZITADEL_JWT_PROFILE_FILE)
+zitadel: Provider = Provider(
+    "zitadel",
+    domain = ZITADEL_DOMAIN,
+    jwt_profile_file = ZITADEL_JWT_PROFILE_FILE
+)
 
-# from pulumi import Config
-# from pulumiverse_zitadel import MachineUser
-
-# MachineUser("testuser", user_name = "testuser", description = "test USER")
-
-# zitadel_config: Config = Config("zitadel")
-# print(zitadel_config.require("jwtProfileFile"))
+MachineUser(
+    "testuser",
+    user_name = "testuser",
+    description = "test USER",
+    opts = ResourceOptions(provider = zitadel)
+)
